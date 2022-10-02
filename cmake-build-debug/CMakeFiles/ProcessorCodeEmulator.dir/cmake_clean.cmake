2224670f610ce393d44b6ddc5e7d8fd82bc4a74f
Wafol <pavel.krauz1@gmail.com>
2022-10-02 19:52:08 +0200

    first commit

---
+++ cmake-build-debug/CMakeFiles/ProcessorCodeEmulator.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProcessorCodeEmulator.dir/main.cpp.obj"
+  "ProcessorCodeEmulator.exe"
+  "ProcessorCodeEmulator.exe.manifest"
+  "ProcessorCodeEmulator.pdb"
+  "libProcessorCodeEmulator.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProcessorCodeEmulator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/ProcessorCodeEmulator.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ProcessorCodeEmulator.
+# This may be replaced when dependencies are built.
